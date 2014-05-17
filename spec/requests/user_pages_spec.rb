dd2fa4d0f088c3dec9eb49b361c2146d172ce98d
mktakuya <takuya.mk96@gmail.com>
2014-05-18 00:55:26 +0900

    Completed 7-6-3

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -28,14 +28,7 @@ describe "UserPages" do
         expect { click_button submit }.not_to change(User, :count)
       end
 
-      describe "after invalid information" do
-        before { click_button submit }
-
-        it { should have_title('Sign up') }
-        it { should have_content('error') }
-      end
-
-      describe "after information" do
+      describe "after submission" do
         before { click_button submit }
 
         it { should have_title('Sign up') }
@@ -53,6 +46,14 @@ describe "UserPages" do
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success') }
       end
     end
   end
